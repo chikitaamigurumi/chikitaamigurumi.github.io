66cc94e6935e9458f36e0523f1d6bc53fec4ba06
chikitaamigurumi <80286051+chikitaamigurumi@users.noreply.github.com>
2021-03-10 15:12:55 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "<link rel="icon" type="image/png" href="images/cb.png">",
+		brand : "images/cb.png",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
